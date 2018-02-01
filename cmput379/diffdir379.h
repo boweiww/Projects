0a11e78fb2e07fbf1a3334693512783618b26dc9
Bowei Wang <bowei2@cs.ualberta.ca>
2018-01-31 20:33:27 -0700

    1/31

---
+++ cmput379/diffdir379.h
@@ -0,0 +1,36 @@
+#include <sys/types.h>
+#include <sys/inotify.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <signal.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define FALSE 0
+#define TRUE 1
+
+struct entry {
+    char *name;
+    int status;
+};
+
+void catch_stop();
+void catch_user();
+void catch_alarm();
+
+void block_signals();
+void restore_signals();
+
+void print_dir();
+
+void print_time();
+
+struct entry *find_entry(char *name, struct entry *arr, int used_entries);
+
+void print_changes();
+
+
